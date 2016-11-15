25930b387b1e728df0106491ee153dfec704cb57
jaSunny <github@sunny.remote.mx>
2016-11-15 21:42:50 +0100

    adding settings for Reverse Port Forwarding

---
+++ setup_ssh_tunnel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+### Setup Port Forwarding
+iptables -t nat -A POSTROUTING -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
+echo 1 > /proc/sys/net/ipv4/ip_forward
+#
+### Adding routings
+iptables -t nat -A PREROUTING -p tcp --dport 7041 -j DNAT --to-destination 10.8.0.3:22 #consumer
+iptables -t nat -A PREROUTING -p tcp --dport 7042 -j DNAT --to-destination 10.8.0.4:22 #ui
+iptables -t nat -A PREROUTING -p tcp --dport 7045 -j DNAT --to-destination 10.8.0.5:22 #producer
+iptables -t nat -A PREROUTING -p tcp --dport 7046 -j DNAT --to-destination 10.8.0.2:22 #marketplace
+#
+iptables-save
+#
+### Delete nat rules if necessary
+#sudo iptables -t nat -F
+#sudo iptables -L -t nat --line-numbers
+#sudo iptables -t nat -D PREROUTING 4
