7df2b3ed3e1fdbdf8167d6ba9bd124a9500362d2
jaSunny <github@sunny.remote.mx>
2016-11-17 09:01:48 +0100

    assigning static IPs for VPN and therefore alter iptables

--- setup_ssh_tunnel.sh
+++ setup_ssh_tunnel.sh
@@ -6,10 +6,11 @@ sysctl -w net.ipv4.ip_forward=1
 echo 1 > /proc/sys/net/ipv4/ip_forward
 #
 ### Adding routings
-iptables -t nat -A PREROUTING -p tcp --dport 7041 -j DNAT --to-destination 10.8.0.3:22 #consumer
-iptables -t nat -A PREROUTING -p tcp --dport 7042 -j DNAT --to-destination 10.8.0.4:22 #ui
-iptables -t nat -A PREROUTING -p tcp --dport 7045 -j DNAT --to-destination 10.8.0.5:22 #producer
-iptables -t nat -A PREROUTING -p tcp --dport 7046 -j DNAT --to-destination 10.8.0.2:22 #marketplace
+iptables -t nat -A PREROUTING -p tcp --dport 7041 -j DNAT --to-destination 10.8.0.20:22 #consumer
+iptables -t nat -A PREROUTING -p tcp --dport 7046 -j DNAT --to-destination 10.8.0.25:22 #marketplace
+iptables -t nat -A PREROUTING -p tcp --dport 7047 -j DNAT --to-destination 10.8.0.21:22 #merchant
+iptables -t nat -A PREROUTING -p tcp --dport 7042 -j DNAT --to-destination 10.8.0.23:22 #ui
+iptables -t nat -A PREROUTING -p tcp --dport 7045 -j DNAT --to-destination 10.8.0.22:22 #producer
 #
 iptables-save
 #
