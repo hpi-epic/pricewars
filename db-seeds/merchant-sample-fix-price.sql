052980e47d6edd3f068b3852aa7962aef3fa69f3
jaSunny <github@sunny.remote.mx>
2017-02-21 13:05:00 +0100

    updating api specs and removing gitsubmodules

--- db-seeds/merchant-sample-fix-price.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.merchants (merchant_id, merchant_token, api_endpoint_url, merchant_name, algorithm_name) VALUES 
-	('uJCuCWUQb4JjREFmy5cBNV6mQ5mKrPY1XDT1LKAkGtY=', 
-	 'Qi5FFXq4rEXT6xqOqCrET12VSS26DKmd2rsHPjMHxtl4rcRioJwIKyomRpsdAfHD', 
-	 'http://merchant-sample-fix-price:5007', 
-	 'Fix Price', 
-	 'E') 
-ON CONFLICT DO NOTHING;
