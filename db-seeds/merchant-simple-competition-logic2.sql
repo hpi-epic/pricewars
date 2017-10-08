45b52c8bd6f01a3c28f0d983f594f82426d6f742
Carsten Walther <carsten.walther@student.hpi.de>
2017-10-08 21:08:44 +0200

    remove competition logic merchants

--- db-seeds/merchant-simple-competition-logic2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.merchants (merchant_id, merchant_token, api_endpoint_url, merchant_name, algorithm_name) VALUES 
-	('1u1yKThClnz/YhCO25bOgs2TPfsZ+Mdd8SA9EIZVSS0=', 
-	 'mNq3VfVIs5gNES1EB6MsKhiNCL7h32zpPqsvWNDPStmhNnvYtIRE0BbCViuYktOA', 
-	 'http://merchant-simple-competition-logic2:5002', 
-	 'Competition Logic 2', 
-	 'Simple') 
-ON CONFLICT DO NOTHING;
