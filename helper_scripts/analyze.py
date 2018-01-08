f60f8239c8a8733ec96e95f53ef12c4ffb8a5e6b
Carsten Walther <carsten.walther@student.hpi.de>
2018-01-08 17:10:06 +0100

    output holding cost, order cost and revenue

--- helper_scripts/analyze.py
+++ helper_scripts/analyze.py
@@ -1,16 +1,12 @@
 """
-Reads the dump of kafka data, that was created by benchmark.py, analyzes the data and
-saves the results to the same directory
+Analyzes the dump of kafka data, that was created by benchmark.py.
+Results (e.g. a merchant's profit and revenue) are saved to a CSV file.
 """
 
 import csv
 import os
 import json
-
-
-def analyze_kafka_dump(directory):
-    merchant_id_mapping = load_merchant_id_mapping(directory)
-    calculate_profit(directory, merchant_id_mapping)
+from collections import defaultdict
 
 
 def load_merchant_id_mapping(directory):
@@ -18,27 +14,30 @@ def load_merchant_id_mapping(directory):
         return json.load(file)
 
 
-def calculate_profit(directory, merchant_id_mapping):
-    profit = {}
-    for merchant_name in merchant_id_mapping.values():
-        profit[merchant_name] = 0
+def analyze_kafka_dump(directory):
+    merchant_id_mapping = load_merchant_id_mapping(directory)
 
+    revenue = defaultdict(float)
     with open(os.path.join(directory, 'kafka', 'buyOffer')) as file:
         for event in json.load(file):
-            merchant_name = merchant_id_mapping[event['merchant_id']]
-            profit[merchant_name] += event['amount'] * event['price']
+            revenue[event['merchant_id']] += event['amount'] * event['price']
 
+    holding_cost = defaultdict(float)
     with open(os.path.join(directory, 'kafka', 'holding_cost')) as file:
         for event in json.load(file):
-            merchant_name = merchant_id_mapping[event['merchant_id']]
-            profit[merchant_name] -= event['cost']
+            holding_cost[event['merchant_id']] += event['cost']
 
+    order_cost = defaultdict(float)
     with open(os.path.join(directory, 'kafka', 'producer')) as file:
         for event in json.load(file):
-            merchant_name = merchant_id_mapping[event['merchant_id']]
-            profit[merchant_name] -= event['billing_amount']
+            order_cost[event['merchant_id']] += event['billing_amount']
 
-    with open(os.path.join(directory, 'profit.csv'), 'w') as file:
+    profit = {merchant_id: revenue[merchant_id] - holding_cost[merchant_id] - order_cost[merchant_id]
+              for merchant_id in merchant_id_mapping}
+
+    with open(os.path.join(directory, 'results.csv'), 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(['name', 'profit'])
-        writer.writerows(profit.items())
+        writer.writerow(['name', 'revenue', 'holding_cost', 'order_cost', 'profit'])
+        for merchant_id in sorted(merchant_id_mapping, key=merchant_id_mapping.get):
+            writer.writerow([merchant_id_mapping[merchant_id], revenue[merchant_id], holding_cost[merchant_id],
+                             order_cost[merchant_id], profit[merchant_id]])
