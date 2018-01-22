4b7b48f914ee839f36ea4d476fea8034ed4c301a
Carsten Walther <carsten.walther@student.hpi.de>
2018-01-22 14:19:22 +0100

    remove db-seeds directory

--- db-seeds/merchant-sample-second-cheapest.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.merchants (merchant_id, merchant_token, api_endpoint_url, merchant_name, algorithm_name) VALUES 
-	('QXY044ASodJNz37NG1aXMPZiz3ZOdJerSRZ5LT2HGRk=', 
-	 'Qrlo6DEkG9SXNIjCByXklvpjbkbQC4scveS2rfm8oEfCWPNhOuSAqIsS4UtGYmsU', 
-	 'http://merchant-sample-second-cheapest:5004', 
-	 'Second Cheapest', 
-	 'B') 
-ON CONFLICT DO NOTHING;
--- db-seeds/merchant-sample-two-bound.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.merchants (merchant_id, merchant_token, api_endpoint_url, merchant_name, algorithm_name) VALUES 
-	('9A3CHpwf97TY4X6F+rDKcgirVfXZQ1HebiyDQQf4LVo=', 
-	 'EGkdMWbvFuVy9w7ReCExgOiyoxr6RyFfF0JbXuXmp4ly1sRf5fdl4MjsKbylbERq', 
-	 'http://merchant-sample-two-bound:5006', 
-	 'Two Bound', 
-	 'D') 
-ON CONFLICT DO NOTHING;
