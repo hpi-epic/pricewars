324bf032f362528a27c019b57f4afd5dac78fc00
Christian Flach <cmfcmf.flach@gmail.com>
2018-06-25 15:41:49 +0200

    Delete helper scripts (they are almost duplicated in the README)

--- helper_scripts/clean_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This file is intended to be used in case of docker problems. It stops and
-# removes all docker containers, followed by a docker-compose up.
-
-docker-compose rm -v -s -f
-docker-compose up
--- helper_scripts/full_clean_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This file is intended to be used in case of severe docker problems. It stops
-# and removes all docker containers, rebuilds them and executes docker-compose up
-# (leading to a time consuming rebuild).
-
-docker-compose rm -v -s -f
-docker-compose up --build
